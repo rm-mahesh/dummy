38db4ce244096029cf89309d43f79d71d4349b22
maheshrm <maheshrm2002@gmail.com>
2025-02-20 22:19:13 +0530

    inline secrets

--- printex.py
+++ printex.py
@@ -2,9 +2,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv("secrets.env")
-
+print("*******SECRETS FROM ENV FILE********")
 secret1=os.getenv("SECRET1")
 secret2=os.getenv("SECRET2")
 
 
 print(secret1,secret2)
+print("*************************************")
+print("*******SECRETS FROM inline actions********")
+secret3=os.getenv("DB_USER")
+secret4=os.getenv("DB_PASS")
+
+
+print(secret3,secret4)
+print("*************************************")
