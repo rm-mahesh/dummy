8fca9dc931d7bf889012521240f3bc4818e21eb2
maheshrm <maheshrm2002@gmail.com>
2025-02-21 17:14:05 +0530

    edited githubsec

--- printex.py
+++ printex.py
@@ -2,20 +2,20 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv("secrets.env")
-print("*******SECRETS FROM ENV FILE********")
-secret1=os.getenv("SECRET1")
-secret2=os.getenv("SECRET2")
+# print("*******SECRETS FROM ENV FILE********")
+# secret1=os.getenv("SECRET1")
+# secret2=os.getenv("SECRET2")
 
 
-print(secret1,secret2)
-print("*************************************")
-print("*******SECRETS FROM inline actions********")
-secret3=os.environ.get("DB_USER")
-secret4=os.environ.get("DB_PASS")
+# print(secret1,secret2)
+# print("*************************************")
+# print("*******SECRETS FROM inline actions********")
+# secret3=os.environ.get("DB_USER")
+# secret4=os.environ.get("DB_PASS")
 
 
-print(secret3,secret4)
-print("*************************************")
+# print(secret3,secret4)
+# print("*************************************")
 print("******* SECRETS FROM GITHUB ACTIONS ********")
 
 DB_USER = os.environ.get("TEST_SECRET_1")
